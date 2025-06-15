61503599cdae056d9f6d14bceb0abc8ee433adf1
I am <88476416+PrimeSolar@users.noreply.github.com>
2025-06-15 22:56:57 +0300

    Update elements.js
    * Implement the dynamic update of the dates on the website using "Date" objects.
    * Update comments for better maintainability and readability of the source code.
    * Update the classes of links for their improved display.
    * Create an identifier for the Scroll to Top Button for its improved display.

--- scripts/elements.js
+++ scripts/elements.js
@@ -3,83 +3,100 @@
 // that can be used across different parts of the project. These elements go beyond what browsers provide,
 // allowing for expanded capabilities and functionality in the project.
 
-// Navigation Bar
+// The Navigation Bar
 navigationBarContainer = document.querySelector("#navigationBarContainer");
 
 function navigationBarContainerF(navigationBarContainer) {
   if (navigationBarContainer != null) {
-    // Define navigation bar content:
+    // Define the navigation bar content:
     navigationBarContainer.innerHTML += `
     <div class="nav-scroller py-1 mb-4 border-bottom">
       <nav class="nav nav-underline justify-content-around">
-        <a class="nav-item nav-link link-body-emphasis active" href="#">World</a>
-        <a class="nav-item nav-link link-body-emphasis" href="#">Country</a>
-        <a class="nav-item nav-link link-body-emphasis" href="#">Technology</a>
-        <a class="nav-item nav-link link-body-emphasis" href="#">Design</a>
-        <a class="nav-item nav-link link-body-emphasis" href="#">Culture</a>
-        <a class="nav-item nav-link link-body-emphasis" href="#">Business</a>
-        <a class="nav-item nav-link link-body-emphasis" href="#">Politics</a>
-        <a class="nav-item nav-link link-body-emphasis" href="#">Science</a>
-        <a class="nav-item nav-link link-body-emphasis" href="#">Health</a>
-        <a class="nav-item nav-link link-body-emphasis" href="#">Style</a>
-        <a class="nav-item nav-link link-body-emphasis" href="#">Travel</a>
+        <a class="nav-item nav-link active" href="#">World</a>
+        <a class="nav-item nav-link" href="#">Country</a>
+        <a class="nav-item nav-link" href="#">Technology</a>
+        <a class="nav-item nav-link" href="#">Design</a>
+        <a class="nav-item nav-link" href="#">Culture</a>
+        <a class="nav-item nav-link" href="#">Business</a>
+        <a class="nav-item nav-link" href="#">Politics</a>
+        <a class="nav-item nav-link" href="#">Science</a>
+        <a class="nav-item nav-link" href="#">Health</a>
+        <a class="nav-item nav-link" href="#">Style</a>
+        <a class="nav-item nav-link" href="#">Travel</a>
       </nav>
     </div>
   </div>
-      `
+      `;
   }
 }
 navigationBarContainerF(navigationBarContainer);
 
-// Scroll to Top Button
+// The Scroll to Top Button
 class ToTop extends HTMLElement {
   connectedCallback() {
     this.innerHTML += `
-    <a href="#" aria-label="Scroll to top" title="Scroll to top">
+    <a href="#" id="to-top" aria-label="Scroll to top" title="Scroll to top">
       <svg width="45px" height="45px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M6 15L12 9L18 15" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
     </a>
-    `
+    `;
   }
 }
 customElements.define("to-top", ToTop);
 
-const toTop = document.querySelector("body").appendChild(document.createElement("to-top"));
+const toTop = document
+  .querySelector("body")
+  .appendChild(document.createElement("to-top"));
 window.addEventListener("scroll", () => {
   if (window.pageYOffset > 100) {
     toTop.classList.add("active");
   } else {
     toTop.classList.remove("active");
   }
-})
+});
 
-// Check Circle
-class CheckCircle extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML += `
-    <img alt="Check circle" title="Included" id="check-circle" name="check-circle" src="assets/check-circle.svg" />
-    `
-  }
-}
-customElements.define("check-circle", CheckCircle);
-
-// Footer
+// The Footer
 footerContainer = document.querySelector("#footerContainer");
 const year = new Date().getFullYear();
+const month = new Date().getMonth();
+const day = new Date().getUTCDate();
+for (let x of document.querySelectorAll("year")) {
+  x.textContent = year;
+}
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+for (let x of document.querySelectorAll("month")) {
+  x.textContent = monthNames[month];
+}
+for (let x of document.querySelectorAll("day")) {
+  x.textContent = day;
+}
 
 function footerContainerF(footerContainer) {
   if (footerContainer != null) {
-    // Define footer content:
+    // Define the footer content:
     footerContainer.innerHTML += `
     <div class="container pt-1 mt-1">
       <footer class="pt-1 mt-5 mb-3">
         <ul class="nav justify-content-center pb-3 mb-3">
         </ul>
-        <p class="text-center pt-3 border-top text-body-secondary">Copyright © <a href="https://primesolar.github.io/web-developer/">Vladislav Kazantsev</a> ${year}</p>
+        <p class="text-center pt-3 border-top text-body-secondary">Copyright © <a href="https://primesolar.github.io/web-developer/">Vladislav Kazantsev</a> ${new Date().getFullYear()}</p>
       </footer>
     </div>
-      `
+      `;
   }
 }
 footerContainerF(footerContainer);
